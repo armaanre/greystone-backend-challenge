19a053873187248bcf75924f8e9290b99157da35
armaanre <armaan.ravieshwar@gmail.com>
2025-08-16 19:25:15 -0400

    removed redunandant session scope logic

--- app/database.py
+++ app/database.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
-from contextlib import contextmanager
-from typing import Generator
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
@@ -16,16 +14,3 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
     pass
-
-
-@contextmanager
-def session_scope() -> Generator[Session, None, None]:
-    session = SessionLocal()
-    try:
-        yield session
-        session.commit()
-    except Exception:
-        session.rollback()
-        raise
-    finally:
-        session.close()
